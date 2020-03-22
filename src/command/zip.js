aa99689712a6fa279a3495696b109dd6afd80b52
tangtao <tangtao@tangtaodeMacBook-Pro.local>
2020-03-22 09:48:46 +0800

    update the zip code

--- src/command/zip.js
+++ src/command/zip.js
@@ -1,8 +1,8 @@
-const compressing = require('compressing');
-const fsExtra = require('fs-extra');
-import fs from 'fs';
-import { handlerError, handlerInfo } from '../utils';
-import path from 'path';
+const compressing = require("compressing");
+const fsExtra = require("fs-extra");
+import fs from "fs";
+import { handlerError, handlerInfo } from "../utils";
+import path from "path";
 
 /**
  * @desc Zip file/folder. Support zip, tar, gzip.
@@ -12,24 +12,57 @@ import path from 'path';
  * @param option {Object}
  * @returns {Promise<void>}
  */
-const zip = async ({ source, destination, type = 'zip', option = {}}) => {
+const zip = async ({ source, destination, type = "zip", option = {} }) => {
   try {
     await fsExtra.ensureDir(path.dirname(destination));
     const isDirectory = fs.statSync(source).isDirectory();
-    let fileType = 'compressFile';
-    if (isDirectory) {
-      fileType = 'compressDir';
-      if (type === 'gzip') {
-        handlerError(`zip error: Gzip only support compressing a single file. if you want to compress a dir with 'gzip', then you may need 'tgz' instead.`);
-      }
+    if (isCheckGzipType(isDirectory, type)) {
+      return;
     }
-    await compressing[type][fileType](source, destination, option)
-      .catch((e) => {
-        handlerError(`zip error: ${e}`);
-      });
+    if (isCheckDirectorySame(source, destination)) {
+      return;
+    }
+    let fileType = getFileType(isDirectory);
+    await compressing[type][fileType](source, destination, option).catch(e => {
+      handlerError(`zip error: ${e}`);
+    });
     handlerInfo(`success: zip '${source}' to '${destination}'`);
   } catch (e) {
     handlerError(`zip error: ${e}`);
+  }
+
+  /**
+   * to check type is error when the source is directory and the type is gzip
+   * @param {*} isDirectory directory is true or false
+   * @param {*} type type of compressing
+   */
+  function isCheckGzipType(isDirectory, type) {
+    let result = true;
+    if (isDirectory && type === "gzip") {
+      handlerError(
+        `zip error: Gzip only support compressing a single file. if you want to compress a dir with 'gzip', then you may need 'tgz' instead.`
+      );
+      result = false;
+    }
+    return result;
+  }
+
+  function isCheckDirectorySame(source, destination) {
+    let result = true;
+    let parseInfo = path.parse(source);
+    let sourceDirectory = parseInfo.dir + path.sep + parseInfo.base;
+    let destDirectory = path.dirname(destination);
+    if (sourceDirectory === destDirectory) {
+      handlerError(
+        `zip error: If the type of compression is a folder, the original and target directories passed in cannot be the same`
+      );
+      result = false;
+    }
+    return result;
+  }
+
+  function getFileType(isDirectory) {
+    return isDirectory ? "compressDir" : "compressFile";
   }
 };
 
