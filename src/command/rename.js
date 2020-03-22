4ddd598076531aac06a37473a1d874c5943566a8
tangtao <tangtao@tangtaodeMacBook-Pro.local>
2020-03-22 09:48:19 +0800

    delete the times

--- src/command/rename.js
+++ src/command/rename.js
@@ -1,29 +1,23 @@
 import fs from 'fs';
 import { handlerError, handlerInfo } from '../utils';
 
-let times = 0;
-
 const rename = ({ source, destination }) => {
   if (!fs.existsSync(source)) {
     handlerError(`rename error: '${source}' is not found in path`);
+    return;
   }
   let src = source.slice(0, source.lastIndexOf('/'));
   let dst = destination.slice(0, destination.lastIndexOf('/'));
   if (src !== dst) {
     handlerError(`rename error: '${source}' and ${destination} are not in the same dir`);
+    return;
   }
 
   try {
     fs.renameSync(source, destination);
     handlerInfo(`success: rename '${source}' to '${destination}'`);
   } catch (e) {
-    if (times < 3) {
-      handlerInfo(`rename notice: rename '${source}' to '${destination}', this is the ${times}th retry`);
-      fs.renameSync(source, destination);
-      times += 1;
-    } else {
-      handlerError(`rename error: rename '${source}' to '${destination}' failed`);
-    }
+    handlerError(`rename error: rename '${source}' to '${destination}' failed ${e}`);
   }
 };
 
